cffc7071be3db3287319debdcff4f2455be5cc13
猫猫球 <45891684+FanSun01@users.noreply.github.com>
2022-07-05 01:03:49 +0800

    先睡觉，明天上班

--- equipmentDemo/src/app/pages/equipment-creation/equipment-creation.component.ts
+++ equipmentDemo/src/app/pages/equipment-creation/equipment-creation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CanComponentLeave } from 'src/app/guards/unsave.guard';
+import { Equipment } from 'src/app/shared/model';
 
 @Component({
   selector: 'app-equipment-creation',
@@ -8,16 +9,32 @@ import { CanComponentLeave } from 'src/app/guards/unsave.guard';
   styleUrls: ['./equipment-creation.component.scss'],
 })
 export class EquipmentCreationComponent implements OnInit, CanComponentLeave {
-  myForm: FormGroup = new FormGroup({
-    username: new FormControl(),
+  equipmentForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    model: new FormControl(),
+    brand: new FormControl(),
+    weight: new FormControl(),
+    manufactureDate: new FormControl(),
   });
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.equipmentForm.get('id')?.disable();
+  }
+
+  onSubmit() {
+    let submit = new Equipment();
+    submit.id = this.equipmentForm.get('id')?.value;
+    submit.model = this.equipmentForm.get('model')?.value;
+    submit.brand = this.equipmentForm.get('brand')?.value;
+    submit.weight = this.equipmentForm.get('weight')?.value;
+    submit.manufactureDate = this.equipmentForm.get('manufactureDate')?.value;
+    console.log(submit);
+  }
 
   canLeave(): boolean {
-    if (this.myForm.dirty) {
+    if (this.equipmentForm.dirty) {
       if (window.confirm('确定要离开吗？')) {
         return true;
       } else {
